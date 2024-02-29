cf8fc913b79a5afb42383beccb2db2d6a3ab3730
reosfire <2.com9761721@gmail.com>
2024-02-29 12:44:03 +0300

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "ru.reosfire"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+kotlin {
+    jvmToolchain(21)
+}
