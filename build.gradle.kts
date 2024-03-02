f13609224efb05b8612928100b3532891a1e41bf
reosfire <2.com9761721@gmail.com>
2024-03-02 18:28:13 +0300

    mongo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktorVersion: String by project
 val kotlinVersion: String by project
 val logbackVersion: String by project
+val kmongoVersion: String by project
+val commonsCodecVersion: String by project
 
 
 plugins {
@@ -20,9 +22,23 @@ repositories {
 }
 
 dependencies {
+    // Ktor
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+
+    // Logging
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    // Mongodb
+    implementation("org.litote.kmongo:kmongo:$kmongoVersion")
+    implementation("org.litote.kmongo:kmongo-coroutine:$kmongoVersion")
+
+    // Codec
+    implementation("commons-codec:commons-codec:$commonsCodecVersion")
 }
 
 application {
--- src/main/kotlin/ru/reosfire/money/manage/Startup.kt
+++ src/main/kotlin/ru/reosfire/money/manage/Startup.kt
@@ -1,29 +1,62 @@
 package ru.reosfire.money.manage
 
+import com.mongodb.ConnectionString
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.reactive.awaitFirst
+import org.bson.Document
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
-fun main() {
+fun connectionStringFromEnv(): ConnectionString {
+    val user = System.getenv("MONGO_USER")
+    val password = System.getenv("MONGO_PASSWORD")
+    val host = System.getenv("MONGO_HOST") // localhost:27017
+
+    return ConnectionString("mongodb://$user:$password@$host/")
+}
+
+data class User(
+    val login: String
+)
+
+suspend fun db() {
+    val database = "MoneyManage"
+
+    val db = KMongo.createClient(
+        connectionStringFromEnv()
+    ).coroutine.getDatabase(database)
+
+    val usersCollection = db.getCollection<User>()
+    usersCollection.insertOne(User("reosfire"))
+}
+
+suspend fun main() {
+    db()
+
     val server = embeddedServer(
         factory = Netty,
         port = 80,
         host = "0.0.0.0"
     ) {
-        setupVue()
+        setupSecurity()
+        setupContentNegotiation()
     }
 
     server.start(wait = true)
 }
 
-fun Application.setupVue() {
-    routing {
-        singlePageApplication {
-            useResources = true
-            defaultPage = "index.html"
-            vue("web-app")
-        }
+fun Application.setupContentNegotiation() {
+    install(ContentNegotiation) {
+        json()
     }
+}
+
+fun Application.setupSecurity() {
+
 }
